67204aee554399ba2722c412bc02eb14dc6e0ef0
emiliosa <emilianoabarca84@gmail.com>
2018-11-30 17:44:38 -0300

    Update conf.js

--- scripts/conf.js
+++ scripts/conf.js
@@ -2,8 +2,8 @@ var SPOTCONTROL = {
     expiredTime: '1440', // Time to expired the local data in minutes
     sessionTime: '1440', // Time to expired user session in minutes
     keyPhrase: 'saww33scomdlo7', // Method sha256
-    domainPath: '/',
+    domainPath: '/spotcontrol/',
     API: 'https://app.maxtracker.com/api/v1/spotcontrolws/',
-    domain: 'http://maxtrackerpwa.local' + '/', // domain + domainPath
+    domain: 'https://emiliosa.github.io' + '/', // domain + domainPath
 
 };
